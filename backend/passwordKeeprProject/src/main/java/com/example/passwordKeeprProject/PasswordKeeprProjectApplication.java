2410ee13b63f2df56d9b961f5f7c43461bac9b89
Jacoby <jallen2033@gmail.com>
2021-09-30 19:45:29 -0700

    set up our Java backend with a simple hello world endpoint

---
+++ backend/passwordKeeprProject/src/main/java/com/example/passwordKeeprProject/PasswordKeeprProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.passwordKeeprProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordKeeprProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		SpringApplication.run(PasswordKeeprProjectApplication.class, args);
+	}
+
+}
