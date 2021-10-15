3fc1bda515f733e2f3674f18aec8d882eee33ff1
Jacoby <jallen2033@gmail.com>
2021-10-14 17:17:50 -0700

    modified register component to handle user registration and auto login + error handling with react toastify

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -108,18 +108,6 @@ function SignIn() {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
             <Box mt={5}>
               <Copyright />
             </Box>
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,17 +1,19 @@
 
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Copyright() {
   return (
@@ -57,8 +59,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Register() {
+// https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
+function Register({ setCurrentUserUuid }) {
   const classes = useStyles();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirm, setPasswordConfirm] = useState('')
+
+  const registerUser = function (event, setCurrentUserUuid) {
+    event.preventDefault()
+    axios.post("http://localhost:8080/register", { email, password, passwordConfirm })
+      .then((response) => {
+        console.log("Response from server: ", response.data)
+        window.localStorage.setItem('Uuid', response.datad)
+        setCurrentUserUuid({ uuid: response.data})
+      }).catch((error) => {
+        console.log(error.response.data.message)
+        toast.error(error.response.data.message)
+      })
+  }
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -66,9 +85,6 @@ function Register() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            {/* <LockOutlinedIcon /> */}
-          </Avatar>
           <Typography component="h1" variant="h5">
             Register
           </Typography>
@@ -83,6 +99,9 @@ function Register() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={(event) => {
+                setEmail(event.target.value)
+              }}
             />
             <TextField
               variant="outlined"
@@ -94,17 +113,23 @@ function Register() {
               type="password"
               id="password"
               autoComplete="current-password"
+              onChange={(event) => {
+                setPassword(event.target.value)
+              }}
             />
             <TextField
               variant="outlined"
               margin="normal"
               required
               fullWidth
-              name="confirmPassword"
-              label="Confirm Password"
-              type="confirmPassword"
-              id="confirmPassword"
+              name="password"
+              label="password"
+              type="password"
+              id="password"
               autoComplete="confirm-current-password"
+              onChange={(event) => {
+                setPasswordConfirm(event.target.value)
+              }}
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -116,18 +141,10 @@ function Register() {
               variant="contained"
               color="primary"
               className={classes.submit}
+              onClick={(event) => registerUser(event, setCurrentUserUuid)}
             >
               Register
             </Button>
-            <Grid container>
-              <Grid item xs>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign In"}
-                </Link>
-              </Grid>
-            </Grid>
             <Box mt={5}>
               <Copyright />
             </Box>
